f1a3fc9b335ffa211da432232f2d6dbc96d8be2b
Juan Kuga <juankp3@gmail.com>
2021-09-10 00:50:26 -0500

    Fix card

--- app_grid/lib/widget/card_category.dart
+++ app_grid/lib/widget/card_category.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 
 class CardCategory extends StatelessWidget {
@@ -5,30 +7,65 @@ class CardCategory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // print("aaaaa");
-    // print(MediaQuery.of(context).size.width / (MediaQuery.of(context).size.height ));
     return Padding(
-      padding: const EdgeInsets.only(left: 30.0, right: 30.0, top: 0),
+      padding: const EdgeInsets.only(left: 30.0, right: 30.0, top: 0, bottom: 20.0),
       child: Container(
-        child: GridView.count(
-          crossAxisCount: 2,
-          controller:  ScrollController(keepScrollOffset: false),
-          shrinkWrap: true,
-          childAspectRatio: MediaQuery.of(context).size.width / (MediaQuery.of(context).size.height ),
-          // childAspectRatio: 0.781890482398957,
-          scrollDirection: Axis.vertical,
-          children: List.generate(30, (index){
-            return _getCard();
-          }),
+        child: Column(
+          children: [
+            // _getCard(),
+            // _getGridCard(itemHeight, itemWidth)
+            _getGridCard2()
+          ],
         ),
       ),
     );
   }
 
+
+
+  Widget _getGridCard(itemHeight, itemWidth){
+    return GridView.count(
+      crossAxisCount: 2,
+      controller:  ScrollController(keepScrollOffset: false),
+      shrinkWrap: true,
+
+      // childAspectRatio: MediaQuery.of(context).size.width / ((MediaQuery.of(context).size.height ) / 2),
+      // childAspectRatio:  0.501,
+      childAspectRatio: (itemWidth / itemHeight),
+      mainAxisSpacing: 0.0,
+      crossAxisSpacing: 50.0,
+      scrollDirection: Axis.vertical,
+      children: List.generate(30, (index){
+        return _getCard();
+      }),
+    );
+  }
+
+  Widget _getGridCard2(){
+    return GridView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 16.0,
+        mainAxisExtent: 220
+      ),
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Container(
+          // color: Colors.blue,
+          child: _getCard(),
+        );
+      },
+    );
+  }
+
+
   Widget _getCard(){
     return Card(
-      elevation: 10.0,
-      margin: EdgeInsets.all(15),
+      elevation: 1.5,
+      // margin: EdgeInsets.all(0),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20.0),
       ),
@@ -38,57 +75,50 @@ class CardCategory extends StatelessWidget {
     );
   }
 
+
   Widget _cardTipo2() {
-    return Container(
-        // width: 10.0,
-        // height: 100.0,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: double.infinity,
-                height: 180.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("jkuga.jpg"),
-                    fit: BoxFit.cover,
-                  ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15.0),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 120.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("jkuga.jpg"),
+                  fit: BoxFit.cover,
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.only(
-                  top: 5.0,
-                  left: 5.0,
-                  right: 5.0,
-                ),
-                child: Text(
-                  "Trading",
-                  style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 20.0,
+                left: 16.0,
+                right: 5.0,
+              ),
+              child: Text(
+                "Coding",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 5.0,
+                left: 16.0,
+                right: 5.0,
+              ),
+              child: Text(
+                "12 Courses",
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.black38,
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.only(
-                  top: 5.0,
-                  left: 5.0,
-                  right: 5.0,
-                ),
-                child: Text(
-                  "Este es un sub title es un sub title es un sub title sfs",
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    fontSize: 11.0,
-                    color: Colors.black38,
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       );
   }
